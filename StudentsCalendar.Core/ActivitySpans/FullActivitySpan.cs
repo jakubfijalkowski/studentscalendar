0216209c887dac9aa08da8c8c120cb2e723570c1
Jakub Fijałkowski <fiolek94@gmail.com>
2014-10-31 21:41:46 +0100

    Define activity span that is always active.

---
+++ StudentsCalendar.Core/ActivitySpans/FullActivitySpan.cs
@@ -0,0 +1,23 @@
+﻿using NodaTime;
+
+namespace StudentsCalendar.Core.ActivitySpans
+{
+	/// <summary>
+	/// Przedział aktywności, który dla dowolnej daty jest aktywny.
+	/// </summary>
+	public sealed class FullActivitySpan
+		: IActivitySpan, IWeekActivitySpan
+	{
+		/// <inheritdoc />
+		public bool IsActive(LocalDate date)
+		{
+			return true;
+		}
+
+		/// <inheritdoc />
+		public bool IsWeekActive(LocalDate date)
+		{
+			return true;
+		}
+	}
+}
