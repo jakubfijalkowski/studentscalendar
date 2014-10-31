b5f61f575e78678110dca3ad8ee385db771ec52c
Jakub Fijałkowski <fiolek94@gmail.com>
2014-10-31 20:13:46 +0100

    Add the notion of "activity spans".

---
+++ StudentsCalendar.Core/IActivitySpan.cs
@@ -0,0 +1,31 @@
+﻿using NodaTime;
+
+namespace StudentsCalendar.Core
+{
+	/// <summary>
+	/// Definicja dziennego przedziału aktywności.
+	/// </summary>
+	public interface IActivitySpan
+	{
+		/// <summary>
+		/// Sprawdza, czy dana data jest aktywna w ramach przedziału aktywności.
+		/// </summary>
+		/// <param name="date">Dzień, który należy sprawdzić.</param>
+		/// <returns></returns>
+		bool IsActive(LocalDate date);
+	}
+
+	/// <summary>
+	/// Definicja tygodniowego przedziału aktywności.
+	/// </summary>
+	public interface IWeekActivitySpan
+		: IActivitySpan
+	{
+		/// <summary>
+		/// Sprawdza, czy dany tydzień jest aktywny.
+		/// </summary>
+		/// <param name="date">Tydzień(reprezentowany przez poniedziałek, który należy sprawdzić.</param>
+		/// <returns></returns>
+		bool IsWeekActive(LocalDate date);
+	}
+}
