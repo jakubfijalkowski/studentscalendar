c2d57ca2bdfefc4175ffe93a48265c654e675ff4
Jakub Fijałkowski <fiolek94@gmail.com>
2014-11-02 23:59:08 +0100

    Add modifier that allows to cancel classes in date range.

---
+++ StudentsCalendar.Core/Modifiers/CancelClassesInRange.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using NodaTime;
+using StudentsCalendar.Core.Intermediates;
+
+namespace StudentsCalendar.Core.Modifiers
+{
+	/// <summary>
+	/// Odwołuje zajęcia w określonym przedziale dat.
+	/// </summary>
+	public sealed class CancelClassesInRange
+		: ICalendarModifier
+	{
+		/// <summary>
+		/// Pobiera lub zmienia datę, od której zajęcia mają zostać anulowane.
+		/// </summary>
+		public LocalDate StartDate { get; set; }
+
+		/// <summary>
+		/// Pobiera lub zmienia datę, do której zajęcia mają zostać anulowane.
+		/// </summary>
+		public LocalDate EndDate { get; set; }
+
+		/// <inheritdoc />
+		public IntermediateCalendar Apply(IntermediateCalendar data, GenerationContext context)
+		{
+			var days = data.Weeks
+				.SelectMany(w => w.Days)
+				.SkipWhile(d => d.Date < this.StartDate)
+				.TakeWhile(d => d.Date <= this.EndDate);
+			foreach (var day in days)
+			{
+				day.Classes.Clear();
+			}
+			return data;
+		}
+	}
+}
