ff13b2ee55a1c38d4de5993e6baa55472eb7eff7
Jakub Fijałkowski <fiolek94@gmail.com>
2014-11-02 23:52:35 +0100

    Add modifier that allows to change "day of the week".

---
+++ StudentsCalendar.Core/Modifiers/ChangeWeekday.cs
@@ -0,0 +1,29 @@
+﻿using NodaTime;
+using StudentsCalendar.Core.ActivitySpans;
+using StudentsCalendar.Core.Intermediates;
+
+namespace StudentsCalendar.Core.Modifiers
+{
+	/// <summary>
+	/// Podmienia dzień tygodnia na inny. Pozwala na użycie planu z innego dnia tygodnia,
+	/// np. "plan poniedziałkowy w piątek".
+	/// </summary>
+	public sealed class ChangeWeekday
+		: IDayModifier
+	{
+		/// <summary>
+		/// Pobiera lub zmienia dzień tygodnia, na który dzień powinien zostać podmieniony.
+		/// </summary>
+		public IsoDayOfWeek DayOfWeek { get; set; }
+
+		/// <inheritdoc />
+		public IActivitySpan ActivitySpan { get; set; }
+
+		/// <inheritdoc />
+		public IntermediateDay Apply(IntermediateDay data, GenerationContext context)
+		{
+			var dayTemplate = context.Template.WeekTemplate.Days[this.DayOfWeek.ToIndex()];
+			return context.DayGenerator.Generate(data.Date, dayTemplate, context);
+		}
+	}
+}
