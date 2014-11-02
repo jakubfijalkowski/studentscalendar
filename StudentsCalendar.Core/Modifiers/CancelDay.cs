d16e6a428609174e927f451c77b90f5cf4017b03
Jakub Fijałkowski <fiolek94@gmail.com>
2014-11-02 23:44:02 +0100

    Add modifiers that "cancel" day/week.

---
+++ StudentsCalendar.Core/Modifiers/CancelDay.cs
@@ -0,0 +1,22 @@
+﻿using StudentsCalendar.Core.ActivitySpans;
+using StudentsCalendar.Core.Intermediates;
+
+namespace StudentsCalendar.Core.Modifiers
+{
+	/// <summary>
+	/// Odwołuje zajęcia danego dnia.
+	/// </summary>
+	public sealed class CancelDay
+		: IDayModifier
+	{
+		/// <inheritdoc />
+		public IActivitySpan ActivitySpan { get; set; }
+
+		/// <inheritdoc />
+		public IntermediateDay Apply(IntermediateDay data, GenerationContext context)
+		{
+			data.Classes.Clear();
+			return data;
+		}
+	}
+}
---
+++ StudentsCalendar.Core/Modifiers/CancelWeek.cs
@@ -0,0 +1,25 @@
+﻿using StudentsCalendar.Core.ActivitySpans;
+using StudentsCalendar.Core.Intermediates;
+
+namespace StudentsCalendar.Core.Modifiers
+{
+	/// <summary>
+	/// Odwołuje zajęcia w całym tygodniu.
+	/// </summary>
+	public sealed class CancelWeek
+		: IWeekModifier
+	{
+		/// <inheritdoc />
+		public IWeekActivitySpan ActivitySpan { get; set; }
+
+		/// <inheritdoc />
+		public IntermediateWeek Apply(IntermediateWeek data, GenerationContext context)
+		{
+			foreach (var day in data.Days)
+			{
+				day.Classes.Clear();
+			}
+			return data;
+		}
+	}
+}
