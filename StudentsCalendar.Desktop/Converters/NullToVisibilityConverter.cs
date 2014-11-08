914e1e0c650a4b6faf533470f5a4d2d4937817c5
Jakub Fijałkowski <fiolek94@gmail.com>
2014-11-08 19:21:32 +0100

    Add NullToVisibilityConverter and provide sample view implementation for WindowsViewModel.

---
+++ StudentsCalendar.Desktop/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace StudentsCalendar.Desktop.Converters
+{
+	/// <summary>
+	/// Konwertuje <c>null</c> na <see cref="Visibility.Collapsed"/> i nie-<c>null</c> na <see cref="Visibility.Visible"/>.
+	/// </summary>
+	public sealed class NullToVisibilityConverter
+		: IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return value == null ? Visibility.Collapsed : Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
