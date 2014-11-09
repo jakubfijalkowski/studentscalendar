5a0f8eb988179007345f1a6552b2f962cb2cf1f9
Jakub Fijałkowski <fiolek94@gmail.com>
2014-11-09 15:40:48 +0100

    Add the notion of contextual view models.

---
+++ StudentsCalendar.UI/IHaveContext.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentsCalendar.UI
+{
+	/// <summary>
+	/// Określa view modele, które muszą być inicjalizowane dodatkowymi informacjami(np. obiektem do edycji).
+	/// </summary>
+	public interface IHaveContext<TData>
+	{
+		/// <summary>
+		/// Zmienia kontekst obiektu.
+		/// </summary>
+		/// <param name="context"></param>
+		void UpdateContext(TData context);
+	}
+}
--- StudentsCalendar.UI/IShell.cs
+++ StudentsCalendar.UI/IShell.cs
@@ -11,6 +11,9 @@
 		/// <remarks>
 		/// Metoda ta może zarówno podmieniać ekran(Windows Phone) jak i wyświetlać
 		/// "okno", które przykrywa główne okno.
+		/// 
+		/// Jeśli <typeparamref name="TViewModel"/> implementuje interfejs <see cref="IHaveContext{TData}"/>,
+		/// obiekt wywołujący tą metodę jest odpowiedzialny za wywołanie <see cref="IHaveContext{TData}.UpdateContext"/>.
 		/// </remarks>
 		/// <typeparam name="TViewModel"></typeparam>
 		/// <returns>Utworzony ViewModel.</returns>
