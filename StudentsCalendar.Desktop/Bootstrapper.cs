8e4ebc251b6dbadedd5cee249de7073fc59d6887
Jakub Fijałkowski <fiolek94@gmail.com>
2014-11-10 17:43:23 +0100

    Replace Caliburn.Metro with custom implementations.

--- StudentsCalendar.Desktop/Bootstrapper.cs
+++ StudentsCalendar.Desktop/Bootstrapper.cs
@@ -1,5 +1,6 @@
-﻿using Autofac;
-using Caliburn.Metro.Autofac;
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
 using Caliburn.Micro;
 using StudentsCalendar.Core;
 using StudentsCalendar.Core.ActivitySpans;
@@ -14,16 +15,75 @@ namespace StudentsCalendar.Desktop
 	/// Bootstrapper wiążący Caliburn.Micro, Autofaca i MahApps.Metro.
 	/// </summary>
 	sealed class Bootstrapper
-		: CaliburnMetroAutofacBootstrapper<IShell>
+		: BootstrapperBase
 	{
+		private IContainer Container;
+
+		public Bootstrapper()
+		{
+			this.Initialize();
+		}
+
+		/// <inheritdoc />
 		protected override void Configure()
 		{
-			base.Configure();
+			this.BuildContainer();
 
 			ViewLocator.AddSubNamespaceMapping(".UI", ".Desktop");
 		}
 
-		protected override void ConfigureContainer(Autofac.ContainerBuilder builder)
+		/// <inheritdoc />
+		protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+		{
+			this.DisplayRootViewFor<IShell>();
+		}
+
+		/// <inheritdoc />
+		protected override object GetInstance(Type service, string key)
+		{
+			if (string.IsNullOrWhiteSpace(key))
+			{
+				return this.Container.Resolve(service);
+			}
+			else
+			{
+				return this.Container.ResolveNamed(key, service);
+			}
+		}
+
+		/// <inheritdoc />
+		protected override IEnumerable<object> GetAllInstances(Type service)
+		{
+			return this.Container.Resolve(typeof(IEnumerable<>).MakeGenericType(service)) as IEnumerable<object>;
+		}
+
+		/// <inheritdoc />
+		protected override void BuildUp(object instance)
+		{
+			this.Container.InjectProperties(instance);
+		}
+
+		private void BuildContainer()
+		{
+			var builder = new ContainerBuilder();
+			this.RegisterInfrastructure(builder);
+			this.RegisterCore(builder);
+			this.RegisterViewsAndViewModels(builder);
+			this.Container = builder.Build();
+		}
+
+		private void RegisterInfrastructure(ContainerBuilder builder)
+		{
+			builder.RegisterType<EventAggregator>()
+				.AsSelf().AsImplementedInterfaces()
+				.InstancePerLifetimeScope();
+
+			builder.RegisterType<MetroWindowManager>()
+				.AsSelf().AsImplementedInterfaces()
+				.InstancePerLifetimeScope();
+		}
+
+		public void RegisterCore(ContainerBuilder builder)
 		{
 			builder.RegisterAssemblyTypes(typeof(CalendarEngine).Assembly)
 				.Where(c => !c.IsAbstract &&
@@ -31,6 +91,13 @@ namespace StudentsCalendar.Desktop
 					 c.IsInNamespaceOf<ICalendarGenerator>() || c.IsInNamespaceOf<Calendar>() ||
 					 c == typeof(CalendarEngine)))
 				.AsImplementedInterfaces().AsSelf();
+		}
+
+		private void RegisterViewsAndViewModels(ContainerBuilder builder)
+		{
+			builder.RegisterAssemblyTypes(typeof(IShell).Assembly)
+				.Where(c => !c.IsAbstract && c.Name.EndsWith("View") && !c.IsInNamespaceOf<Controls.ClassesView>())
+				.AsSelf();
 
 			builder.RegisterAssemblyTypes(typeof(IShell).Assembly)
 				.Where(c => !c.IsAbstract)
@@ -39,8 +106,6 @@ namespace StudentsCalendar.Desktop
 			builder.RegisterAssemblyTypes(typeof(ShellViewModel).Assembly)
 				.Where(c => !c.IsAbstract && (c.Name.EndsWith("ViewModel") || c.IsInNamespaceOf<Platform.SampleStorage>()))
 				.AsImplementedInterfaces().AsSelf();
-
-			base.ConfigureContainer(builder);
 		}
 	}
 }
---
+++ StudentsCalendar.Desktop/MetroWindowManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using MahApps.Metro.Controls;
+
+namespace StudentsCalendar.Desktop
+{
+	/// <summary>
+	/// Implementacja <see cref="IWindowManager"/> dla MahApps.
+	/// </summary>
+	sealed class MetroWindowManager
+		: WindowManager
+	{
+		protected override Window EnsureWindow(object model, object view, bool isDialog)
+		{
+			MetroWindow window = view as MetroWindow;
+			if (window == null)
+			{
+				window = new MainWindow { Content = view };
+			}
+
+			window.SetValue(View.IsGeneratedProperty, true);
+			var inferredOwner = InferOwnerOf(window);
+			if (inferredOwner != null)
+			{
+				window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+				window.Owner = inferredOwner;
+			}
+			else
+			{
+				window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+			}
+
+			return window;
+		}
+	}
+}
