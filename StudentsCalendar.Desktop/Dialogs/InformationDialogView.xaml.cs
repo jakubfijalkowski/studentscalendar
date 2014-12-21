d5cd7772b31eb3df35e25ab590bf0590d54a489e
Jakub Fijałkowski <fiolek94@gmail.com>
2014-12-21 23:13:07 +0100

    Add view for InformationDialog.

---
+++ StudentsCalendar.Desktop/Dialogs/InformationDialogView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using StudentsCalendar.UI.Dialogs;
+
+namespace StudentsCalendar.Desktop.Dialogs
+{
+	/// <summary>
+	/// Interaction logic for InformationDialogView.xaml
+	/// </summary>
+	[Dialog(typeof(InformationDialog))]
+	public partial class InformationDialogView
+		: BaseDialog, IDialogView
+	{
+		public InformationDialogView()
+		{
+			InitializeComponent();
+		}
+	}
+}
